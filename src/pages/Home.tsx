f9772cf0fca8c699719276caaabcbb8569015fef
gileduardo <gilgea@gmail.com>
2026-06-11 17:23:58 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Header from '@/components/Header';
+import Banner from '@/components/Banner';
+import Search from '@/components/Search';
+import Card from '@/components/Card';
+import type { Filme } from '@/types';
+import dadosFilmes from '@/data/movies.json';
+
+export default function Home() {
+    
+    const [Filmes] = useState<Filme[]>(dadosFilmes);
+
+    return (
+        <div className="min-h-screen bg-black px-4">
+            <Header />
+            <Banner filme={Filmes[0]} />
+            <section className="bg-black pt-12 pb-20">
+                <div className="container">
+                    <div className="mb-12">
+                        <h2 className="text-4xl md:text-5xl font-black text-white mb-4">
+                            Descubra Filmes
+                        </h2>
+                         <Search />
+                    </div>
+            
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                        {
+                            Filmes.map((filme) => (
+                                <Card key={filme.id} movie={filme} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Filme {
+  id: number;
+  titulo: string;
+  descricao: string;
+  imagem: string;
+  fundo: string;
+  avaliacao: number;
+  ano: number;
+  genero: string[];
+  duracao: number;
+  diretor: string;
+}
